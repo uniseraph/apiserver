33e77ba9c06e41c3ec3253749393dc488a5daeef
wuzhengtao <zhengtao.wuzt@gmail.com>
2017-07-14 23:10:28 +0800

    add deployment

--- application/deployment.go
+++ application/deployment.go
@@ -10,20 +10,29 @@ import (
 )
 
 
-func AddDeploymentLog(ctx context.Context,app *types.Application , pool *types.PoolInfo , user *types.User , operation string, opts *types.DeploymentOpts ) {
+func AddDeploymentLog(ctx context.Context,app *types.Application , pool *types.PoolInfo , user *types.User , operation string, opts types.DeploymentOpts ) error {
 
 
 	deployment :=&types.Deployment{
 		Id: bson.NewObjectId(),
 		OperationType: operation,
+		Operator : user.Id.Hex(),
 		ApplicationId: app.Id.Hex(),
 		ApplicationVersion:app.Version,
 		PoolId: pool.Id.Hex(),
 		CreatedTime: time.Now().Unix(),
 		CreatorId: user.Id.Hex(),
+		Opts:opts,
+
 	}
 
 	cb := func(cs map[string]*mgo.Collection)error {
+		colDeployment , _ := cs["deployment"]
+
+		if err := colDeployment.Insert(deployment) ;err!=nil {
+			return err
+
+		}
 		return nil
 	}
 
