6a4594abc93c943730ea58ad25a79e1c6490d4e3
uniseraph <zhengtao.wuzt@gmail.com>
2017-07-14 23:10:25 +0800

    add deployment

--- application/deployment.go
+++ application/deployment.go
@@ -1,29 +1,27 @@
 package application
 
 import (
-	"github.com/zanecloud/apiserver/types"
-	"gopkg.in/mgo.v2/bson"
-	"time"
 	"context"
+	"github.com/zanecloud/apiserver/types"
 	"github.com/zanecloud/apiserver/utils"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"time"
 )
 
+func AddDeploymentLog(ctx context.Context, app *types.Application, pool *types.PoolInfo, user *types.User, operation string, opts *types.DeploymentOpts) error {
 
-func AddDeploymentLog(ctx context.Context,app *types.Application , pool *types.PoolInfo , user *types.User , operation string, opts *types.DeploymentOpts ) {
-
-
-	deployment :=&types.Deployment{
-		Id: bson.NewObjectId(),
-		OperationType: operation,
-		ApplicationId: app.Id.Hex(),
-		ApplicationVersion:app.Version,
-		PoolId: pool.Id.Hex(),
-		CreatedTime: time.Now().Unix(),
-		CreatorId: user.Id.Hex(),
+	deployment := &types.Deployment{
+		Id:                 bson.NewObjectId(),
+		OperationType:      operation,
+		ApplicationId:      app.Id.Hex(),
+		ApplicationVersion: app.Version,
+		PoolId:             pool.Id.Hex(),
+		CreatedTime:        time.Now().Unix(),
+		CreatorId:          user.Id.Hex(),
 	}
 
-	cb := func(cs map[string]*mgo.Collection)error {
+	cb := func(cs map[string]*mgo.Collection) error {
 		return nil
 	}
 
