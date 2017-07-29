6ec374c2b34d304d4ac7744b973b1f2db69b0fef
uniseraph <zhengtao.wuzt@gmail.com>
2017-07-29 14:27:31 +0800

    fix

--- scripts/sbin/install.sh
+++ scripts/sbin/install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 
-BASE_DIR=$(cd `dirname $0` && pwd -P)
+BASE_DIR=$(cd `dirname $0` && cd .. && pwd -P)
 
 
 cp  ${BASE_DIR}/systemd/apiserver.service /etc/systemd/system/
-mkdir -p $BASE_DIR}/etc/zanecloud && cp  systemd/apiserver /etc/zanecloud/apiserver
+mkdir -p ${BASE_DIR}/etc/zanecloud && cp  systemd/apiserver /etc/zanecloud/apiserver
 
 bash ${BASE_DIR}/sbin/init.sh
 ./bin/apiserver init
